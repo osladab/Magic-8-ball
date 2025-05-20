1260dc69ed09efbea8ea9014e75dfba9107942b2
osladab <osladab@gmail.com>
2025-05-20 12:15:19 +0000

    latest

--- CesarCipher.py
+++ CesarCipher.py
@@ -1,4 +1,3 @@
-
 def cipher(text, shift, lang):
     res = ''
     
@@ -38,7 +37,32 @@ def decipher(text, shift, lang):
         return res
 
 def lang_detect(text):
-    ru = 'ru'
-    en = 'en'
+    #ru = 'ru'
+    #en = 'en'
     for c in text:
-        if ord(c.upper()) in range( 
+        if ord(c.upper()) in range(65, 91):
+            return 'en'
+        elif ord(c.ypper()) in range(1040, 1072):
+            return 'ru'
+        else:
+            continue
+
+print('Welcome to Caesar CIpher machine!')
+cont = 'y'
+while cont.lower == 'y':
+    print('Do you want to encode or decode text? For encode enter "e", for decode enter "d"')
+    action = input()
+    print('What text do you want to encode/decode?')
+    text = input()
+    print('Enter shift for coding.')
+    shift = int(input())
+
+    lang = lang_detect(text)
+
+    if action.lower == 'e':
+        print(cipher(text, shift, lang))
+    elif action.lower == 'd':
+        print(decipher(text, shift, lang))
+
+    print('Do you want to continue? y/n')
+    cont = input()
