50fc79cc46f5e6df5cf61cf9792b7b9c724dff83
osladab <osladab@gmail.com>
2025-05-20 11:34:59 +0300

    Update CesarCipher.py

--- CesarCipher.py
+++ CesarCipher.py
@@ -15,3 +15,20 @@ def cipher(text, shift, lang):
             else:
                 res = res + c
         return res
+
+def decipher(text, shift, lang):
+    res = ''
+    if lang == 'ru':
+        for c in text:
+            if c.isalpha():
+                res = res + chr((ord(c) - shift) % 32)
+            else:
+                res = res + c
+        return res
+    elif lang == 'en':
+        for c in text:
+            if c.isalpha():
+                res = res + chr((ord(c) - shift) % 26)
+            else:
+                res = res + c
+        return res
