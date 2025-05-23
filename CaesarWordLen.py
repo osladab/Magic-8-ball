1f9384f4a566f06c777efb53ee0ef58525cd05e3
Федоров Илья <osladab@gmail.com>
2025-05-23 05:58:50 +0300

    2305 final

---
+++ CaesarWordLen.py
@@ -0,0 +1,33 @@
+#Шифрование Цезарем со сдвигом на длину каждого слова беззнаков препинания
+en_al_up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+en_al_lo = 'abcdefghijklmnopqrstuvwxyz'
+
+def cifr(word):
+    word_lst = [w for w in word if w.isalpha()]
+    word_len = ''.join(word_lst)
+    en_al_cifr_up = ''
+    for c in en_al_up:
+        en_al_cifr_up += en_al_up[(en_al_up.index(c) + len(word_len)) % 26]
+    en_al_cifr_lo = ''
+    for c in en_al_lo:
+        en_al_cifr_lo += en_al_lo[(en_al_lo.index(c) + len(word_len)) % 26]
+
+
+
+    res = ''
+    for c in word:
+        if c.isalpha():
+            if c.isupper():
+                res += en_al_cifr_up[en_al_up.index(c) % 26]
+            elif c.islower():
+                res += en_al_cifr_lo[en_al_lo.index(c) % 26]
+        else:
+            res = res + c
+    return res
+
+text = input()
+
+txt_lst = []
+txt_lst = text.split()
+for w in txt_lst:
+    print(cifr(w), end = ' ')
